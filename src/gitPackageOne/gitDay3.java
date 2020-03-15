aabbaf7cc14ef4780b6fbce2e4b1e84ffbff1471
Osman Onder <honestseller9@gmail.com>
2020-03-15 14:05:08 -0500

    second commit from local repo

--- src/gitPackageOne/gitDay3.java
+++ src/gitPackageOne/gitDay3.java
@@ -2,4 +2,19 @@ package gitPackageOne;
 
 public class gitDay3 {
 
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("My first changes on local master");
+		
+	}
+	
+	
+	
+	
+	
 }
